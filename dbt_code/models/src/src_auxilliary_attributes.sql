414b015bfae9ab86488d559a73100f8b33765010
0marsm0 <alish.yuldashev88@gmail.com>
2025-10-22 21:47:26 +0200

    change dbt

--- dbt_code/models/src/src_auxilliary_attributes.sql
+++ dbt_code/models/src/src_auxilliary_attributes.sql
@@ -3,12 +3,10 @@ with stg_job_ads as (
     select * from {{ source('jobtech_analysis', 'stg_ads') }}
 )
 
-   
 select
     id,
     experience_required,
     access_to_own_car,
     driving_license_required
-
 from stg_job_ads
-
+where occupation_field__concept_id in ["apaJ_2ja_LuF", "ASGV_zcE_bWf", "NYW6_mP6_vwf"]
