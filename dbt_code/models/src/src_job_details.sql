06072f17a90edaf808684d713880886add5070eb
0marsm0 <alish.yuldashev88@gmail.com>
2025-10-22 21:47:26 +0200

    change dbt

--- dbt_code/models/src/src_job_details.sql
+++ dbt_code/models/src/src_job_details.sql
@@ -2,20 +2,15 @@ with stg_job_ads as (
     select * from {{ source('jobtech_analysis', 'stg_ads') }}
 )
 
-SELECT
+select
     id,
     headline,
- 
     description__text as description,
     description__text_formatted as description_html_formatted,
-    
     employment_type__label as employment_type,
-
     duration__label as duration,
-    
     salary_type__label as salary_type,
-    
     scope_of_work__min as scope_of_work_min,
     scope_of_work__max as scope_of_work_max
-
-FROM stg_job_ads
+from stg_job_ads
+where occupation_field__concept_id in ["apaJ_2ja_LuF", "ASGV_zcE_bWf", "NYW6_mP6_vwf"]
--- dbt_code/models/src/src_occupation.sql
+++ dbt_code/models/src/src_occupation.sql
@@ -2,14 +2,9 @@ with stg_job_ads as (
     select * from {{ source('jobtech_analysis', 'stg_ads') }}
 )
 
-
-SELECT DISTINCT 
-    
+select distinct 
     occupation__label as occupation,
-
     occupation_group__label as occupation_group,
-  
     occupation_field__label as occupation_field,
-   
-FROM stg_job_ads
-WHERE occupation__label IS NOT NULL
+from stg_job_ads
+where occupation_field__concept_id in ["apaJ_2ja_LuF", "ASGV_zcE_bWf", "NYW6_mP6_vwf"]
