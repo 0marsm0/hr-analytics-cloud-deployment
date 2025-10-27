f5c938a83ee7c90380c941326637addfd861759d
0marsm0 <alish.yuldashev88@gmail.com>
2025-10-27 14:48:09 +0100

    docker

--- dashboard/conn_warehouse.py
+++ dashboard/conn_warehouse.py
@@ -1,14 +1,13 @@
 import os
-from dotenv import load_dotenv
 import pandas as pd
 import duckdb
-from pathlib import Path
+
+# from pathlib import Path
 
 
 def get_job_list(query="SELECT * FROM jontech_analysis.marts.mart_main"):
-    db_path=str(Path(__file__).parents[1]/"data_warehouse/job_ads.duckdb")
+    db_path = "/data_warehouse/job_ads.duckdb"
+    # db_path=str(Path(__file__).parents[1]/"data_warehouse/job_ads.duckdb")
 
     with duckdb.connect(db_path, read_only=True) as conn:
-       return conn.query(f"{query}").df()
-   
-
+        return conn.query(f"{query}").df()
