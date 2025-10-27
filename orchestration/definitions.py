5ed306179827d74714badb00e12c5c2f271d130f
0marsm0 <alish.yuldashev88@gmail.com>
2025-10-27 14:48:09 +0100

    docker

--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -6,16 +6,18 @@ from dagster_dbt import DbtCliResource, DbtProject, dbt_assets
 from dagster import define_asset_job, ScheduleDefinition, AssetSelection
 from dlt_code.snapshot_load_ads import snapshot_ads_source
 from dlt_code.stream_load_ads import stream_ads_source
-import sys
+
+# import sys
 import os
 
-project_root = Path(__file__).parents[1]
-sys.path.insert(0, str(project_root))
-db_path = str(project_root / "data_warehouse/job_ads.duckdb")
+# project_root = Path(__file__).parents[1]
+# sys.path.insert(0, str(project_root))
+# db_path = str(project_root / "data_warehouse/job_ads.duckdb")
+db_path = "/data_warehouse/job_ads.duckdb"
 
 dlt_resource = DagsterDltResource()
 
-dbt_project_dir = str(project_root / "dbt_code")
+dbt_project_dir = "/pipeline/"
 default_dbt_path = Path.home() / ".dbt"
 dbt_profiles_dir = os.getenv("DBT_PROFILES_DIR", default=default_dbt_path)
 
